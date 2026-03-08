7301a146c19836495cfb1859e8b68960ad87667c
Nir <106467254+NirSanasm@users.noreply.github.com>
2026-03-08 20:55:40 +0530

    [1980] [Time Beats: 100.00%] [Memory Beats: 71.26%] - LeetPush

--- DCP-03-26/Find-Unique-Binary-String.py
+++ DCP-03-26/Find-Unique-Binary-String.py
@@ -1,22 +1,9 @@
 1class Solution:
 2    def findDifferentBinaryString(self, nums: List[str]) -> str:
-3        n = len(nums)
-4
-5        list_of_bin = {}
-6
-7        def generate_binary_strings(n, prefix=""):
-8            if n == 0:
-9                list_of_bin[prefix] = "Not Found"
-10            else:
-11                generate_binary_strings(n - 1, prefix + "0")
-12                generate_binary_strings(n - 1, prefix + "1")
-13        
-14        generate_binary_strings(n)
-15
-16        for n in nums:
-17            list_of_bin[n] = "Found"
-18        
-19        for k, v in list_of_bin.items():
-20            if v == "Not Found":
-21                return k
-22        
+3        l=[]
+4        for i in range(len(nums)):
+5            if nums[i][i]=='0':
+6                l.append('1')
+7            else:
+8                l.append('0')
+9        return ''.join(l)
