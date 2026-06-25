7c04bf35af74f78691e563a288417be0e13fc9bb
Nir <106467254+NirSanasm@users.noreply.github.com>
2026-06-25 18:51:58 +0530

    Time: 42 ms (80.15%) | Memory: 19 MB (92.35%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.py
@@ -0,0 +1,23 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        if x < 0:
+            sign = -1
+        else:
+            sign = 1
+        
+        x = abs(x)
+        res = 0
+        while x != 0:
+            val = x % 10
+            res = res * 10 + val
+            x = x // 10
+
+
+        res = res * sign
+
+        if res > 2 ** 31 - 1 or res < -2 ** 31:
+            return 0
+        
+        return res
+        
+       
