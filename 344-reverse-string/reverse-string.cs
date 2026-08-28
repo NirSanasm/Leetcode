04dfd1c83a6b3c9759bea6e38ec48cb0900e4787
Nir <106467254+NirSanasm@users.noreply.github.com>
2026-08-28 18:13:53 +0530

    Time: 0 ms (100.00%) | Memory: 65.6 MB (7.65%) - LeetSync

---
+++ 344-reverse-string/reverse-string.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public void ReverseString(char[] s) {
+        int i = 0;
+        int j = s.Length - 1;
+        
+
+        while(i < j){
+            var temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            i += 1;
+            j -= 1;
+        }
+        
+    }
+}
