97531a78f1750ce6262d68720d04c43b3a491d79
Nir <106467254+NirSanasm@users.noreply.github.com>
2026-09-06 23:13:52 +0530

    Time: 7 ms (52.15%) | Memory: 23.1 MB (31.81%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,18 +1,12 @@
+
+from collections import Counter
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
 
-        hashset = {}
-        
-        for num in nums:
-            if hashset.get(num, None):
-                hashset[num] = hashset[num] + 1
-            else:
-                hashset[num] = 1
 
-        
-        new_list = [k for k, v in sorted(hashset.items(), reverse = True, key=lambda item: item[1])]
 
-        return new_list[:k]
+        freq = Counter(nums)
+        return [num for num, count in freq.most_common(k)]
 
 
 
